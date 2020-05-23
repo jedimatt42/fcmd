421d135828b492ab432658cd14ab55e177d29689
Matthew Splett <jedimatt42@protonmail.com>
2020-05-22 21:21:48 -0700

    move the banner to assembly hex
    tag the .ans files as binary so git doesn't destroy it in the
    future.
    
    Generate assembly source for said .ans using python script.
    document that process.
    
    Fix banner function to use new external data

---
+++ ans2asm.py
@@ -0,0 +1,12 @@
+
+with open('fcbanner.ans', 'rb') as ansi:
+  data = ansi.read()
+  
+with open('b3_fcbanner.asm', 'w') as h:
+  h.write('\tDEF\tbandata\n')
+  h.write('\n')
+  h.write('bandata:\n')
+  for b in data:
+    h.write('\tBYTE\t0x%02x\n' % (b))
+  h.write('\tBYTE\t0x00\n')
+
---
+++ banner.md
@@ -0,0 +1,5 @@
+# creating the ROM banner
+
+You can use an ansi art generated that saves a file in cp437.
+the python script ans2asm.py will convert a file fcbanner.ans to b3_fcbanner.asm
+
