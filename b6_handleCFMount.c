51ffccfa9a05b697d70e6121e011fb3181922467
Matthew Splett <jedimatt42@protonmail.com>
2020-06-06 10:18:59 -0700

    notes on nanopeb volume mounting

---
+++ b6_handleCFMount.c
@@ -0,0 +1,11 @@
+
+
+/*
+ *  VDP Address | Purpose
+ *  ----------- | ----------------------------
+ *  0x3FF8-9    | if 0xAA03 then CF exists
+ *  0x3FFA-B    | volume# of DSK1
+ *  0x3FFC-D    | volume# of DSK2
+ *  0x3FFE-F    | volume# of DSK3
+ */
+
