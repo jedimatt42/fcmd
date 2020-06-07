b5126343c4de7225420970d56a2c71350059bf03
Matthew Splett <jedimatt42@protonmail.com>
2020-06-06 23:07:21 -0700

    remove confusing initialization

--- b7_tipi_msg.c
+++ b7_tipi_msg.c
@@ -1,8 +1,8 @@
 #include "b7_tipi_msg.h"
 
 
-#define GPLWS_R0 *((volatile unsigned int*)0x83E0) 
-#define GPLWS_R1 *((volatile unsigned int*)0x83E2) 
+#define GPLWS_R0 *((volatile unsigned int*)0x83E0)
+#define GPLWS_R1 *((volatile unsigned int*)0x83E2)
 
 void tipi_lib_init();
 
@@ -36,7 +36,7 @@ void tipi_lib_init()
     enableTipi();
     unsigned int* dsrrom = (unsigned int*) 0x4000;
     dsrrom += 4; // 4 words, not bytes
-    
+
     if (*dsrrom != 0) {
       dsrrom = ((unsigned int*) *dsrrom) + 2;
       unsigned char* dsrname = (unsigned char*) dsrrom;
@@ -54,7 +54,7 @@ void tipi_lib_init()
 
 void tipi_recvmsg(unsigned int* len, unsigned char* buf)
 {
-  GPLWS_R0 = (unsigned int)*len;
+  GPLWS_R0 = 0;
   GPLWS_R1 = (unsigned int)buf;
   __asm__("lwpi >83E0\n\tmov @>4010,r4\n\tbl *r4\n\tlwpi >8300");
   *len = GPLWS_R0;
