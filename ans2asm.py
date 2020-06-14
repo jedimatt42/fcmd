f1f7bb403a1ae4a4f4e6683e3ff1f68186dfa31a
Matthew Splett <jedimatt42@protonmail.com>
2020-06-14 12:31:08 -0700

    fix banner, now un-edited outside PabloDraw

--- ans2asm.py
+++ ans2asm.py
@@ -1,14 +1,16 @@
 
 with open('fcbanner.ans', 'rb') as ansi:
-  data = ansi.read()
+    data = ansi.read()
   
 with open('b3_fcbanner.asm', 'w') as h:
-  h.write('\tDEF\tbandata\n')
-  h.write('\n')
-  h.write('bandata:\n')
-  h.write('\tBYTE\t0x0A\n')
-  h.write('\tBYTE\t0x0A\n')
-  for b in data:
-    h.write('\tBYTE\t0x%02x\n' % (b))
-  h.write('\tBYTE\t0x00\n')
+    h.write('\tDEF\tbandata\n')
+    h.write('\n')
+    h.write('bandata:\n')
+    h.write('\tBYTE\t0x0A\n')
+    h.write('\tBYTE\t0x0A\n')
+    for b in data:
+        if b == 0x1A:
+            break
+        h.write('\tBYTE\t0x%02x\n' % (b))
+    h.write('\tBYTE\t0x00\n')
 
