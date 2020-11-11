21c05da28cae4d6ec7fa1bb679c0419432f7fc70
Matthew Splett <jedimatt42@protonmail.com>
2020-11-11 08:18:47 -0800

    mame 220 doesn't like gromemu + rom

--- makerpk.sh
+++ makerpk.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 rm -f FCMD.RPK 
+cp js99er-layout.xml layout.xml
 zip FCMD.RPK layout.xml FCMD8.bin FCMDG.bin
+rm -f MAME-FCMD.RPK
+cp mame-layout.xml layout.xml
+zip MAME-FCMD.RPK layout.xml FCMD8.bin
 
