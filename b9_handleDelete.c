3cf67b9c128ac031c1c83423620cee2ee20b04e9
Matthew Splett <jedimatt42@protonmail.com>
2021-06-05 16:57:21 -0700

    fix delete to work on IDE
    Needed to close the CATALOG before mutating it with delete operations.
    So now we build out a list of matching names to delete into the heap.
    then close the catalog, and iterate over the names and delete them.
    
    probably a good pattern for the other wildcarding operations. However
    those that simply read do not seem to get errors.

--- b9_handleDelete.c
+++ b9_handleDelete.c
@@ -7,12 +7,14 @@
 #include "b1_strutil.h"
 #include "b2_dsrutil.h"
 #include "b2_lvl2.h"
+#include "b0_heap.h"
 #include "b8_terminal.h"
 #include "b8_getstr.h"
 #include <string.h>
 
 static void onIgnoreVolInfo(struct VolInfo *volInfo);
 static void onDeleteDirEntry(struct DirEntry *dirEntry);
+static void deleteQueue(char* filename);
 
 static struct DeviceServiceRoutine *srcdsr;
 static char* srcpath;
@@ -21,10 +23,17 @@ static int filecount;
 static int matched;
 static int yes;
 
+struct __attribute__((__packed__)) MatchedName {
+  char name[11];
+};
+
+static struct MatchedName* names;
+
 void handleDelete() {
   filecount = 0;
   matched = 0;
   srcdsr = 0;
+  names = 0;
 
   // allocate some buffer on the stack, set the global pointers.
   char tmp[256];
@@ -57,6 +66,14 @@ void handleDelete() {
   if (matched == 0) {
     tputs_rom("error, no matching file found.\n");
   } else {
+    for(int idx = 0; idx < matched; idx++) {
+      deleteQueue((names + idx)->name);
+    }
+
+    if (names) {
+      bk_free((char*)names);
+    }
+
     tputs_rom("deleted ");
     bk_tputs_ram(bk_uint2str(filecount));
     tputs_rom(" files.\n");
@@ -80,28 +97,41 @@ static void onDeleteDirEntry(struct DirEntry *dirEntry) {
   if (!bk_globMatches(dirEntry->name)) {
     return;
   }
-  matched = 1;
+
+  struct MatchedName* nameMatch = (struct MatchedName*) bk_alloc(sizeof(struct MatchedName));
+  if (nameMatch == 0) {
+    bk_tputs_ram("out of heap, skipping ");
+    bk_tputs_ram(dirEntry->name);
+    bk_tputc('\n');
+    return;
+  }
+  if (names == 0) {
+    names = nameMatch;
+  }
+
+  bk_strcpy(nameMatch->name, dirEntry->name);
+  matched++;
+}
+
+static void deleteQueue(char* filename) {
+  char path[256];
+  bk_strcpy(path, srcpath);
+  bk_strcat(path, filename);
 
   if (yes) {
     tputs_rom("deleting ");
-    bk_tputs_ram(srcpath);
-    bk_tputs_ram(dirEntry->name);
+    bk_tputs_ram(path);
     bk_tputc('\n');
   } else {
     // prompt to continue
     tputs_rom("delete file ");
-    bk_tputs_ram(srcpath);
-    bk_tputs_ram(dirEntry->name);
+    bk_tputs_ram(path);
     tputs_rom("? (y/n) ");
     int yn = keyYesNo();
     if (!yn) {
       return;
     }
   }
-
-  char path[256];
-  bk_strcpy(path, srcpath);
-  bk_strcat(path, dirEntry->name);
 
   struct PAB pab;
   bk_initPab(&pab);
