4743a467ff38f1624d2da59c2880de11a96cf9d9
Matthew Splett <jedimatt42@protonmail.com>
2021-06-05 16:54:21 -0700

    protect against getting too close to the stack
    users of alloc should check for 0 and handle failure to allocate.
    currently the only other use is in building the DSR entry list, after
    which there is typically still 4k of stack/heap available.
    
    Added bk_free to restore heap to a previous value.
    
    alloc and free really only work if the alloc is perminent, or
    you are freeing the last things allocated.

--- b0_heap.c
+++ b0_heap.c
@@ -8,7 +8,21 @@ extern int __BSS_END;
 char* heap_end = (char*)&__BSS_END;
 
 char* alloc(int sz) {
+  char* stack_addr;
+  __asm__(
+    "mov r10,%0"
+    : "=r" (stack_addr)
+  );
+
+  if (stack_addr - heap_end < 512) {
+    return 0;
+  }
+
   void* result = heap_end;
   heap_end += sz;
   return result;
+}
+
+void free(char* addr) {
+  heap_end = addr;
 }
--- b0_heap.h
+++ b0_heap.h
@@ -4,9 +4,11 @@
 #include "banking.h"
 
 char* alloc(int sz);
+void free(char* addr);
 
 extern char* heap_end;
 
 DECLARE_BANKED(alloc, BANK(0), char*, bk_alloc, (int sz), (sz))
+DECLARE_BANKED_VOID(free, BANK(0), bk_free, (char* addr), (addr))
 
 #endif
