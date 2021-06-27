7f2cd5e65f9f6ecddcd74bede16e5b0ff07345f0
Matthew Splett <jedimatt42@protonmail.com>
2021-06-26 22:31:09 -0700

    basically works, now to make it pretty

--- example/gcc/fcmenu/fcmenu.c
+++ example/gcc/fcmenu/fcmenu.c
@@ -1,5 +1,8 @@
 #include "fc_api.h"
 #include "ioports.h"
+#include "kscan.h"
+
+struct DisplayInformation dinfo;
 
 struct __attribute__((__packed__)) MenuEntry {
   char key;
@@ -11,7 +14,13 @@ int entry_idx;
 int entry_max;
 struct MenuEntry entries[100];
 
+void drawBackdrop();
+void layoutMenu(int entry_offset);
+struct MenuEntry* pickEntry();
+
 int main(char* args) {
+  fc_display_info(&dinfo);
+
   entry_idx = 0;
   entry_max = 0;
   struct DeviceServiceRoutine* dsr;
@@ -43,7 +52,7 @@ int main(char* args) {
 
         vdp_memread(pab.VDPBuffer, buffer, pab.CharCount);
         if (buffer[0] != '#') {
-          // if not blank, and not starts with # comment
+          // if not blank, and not starts with # comment, build an entry
           entries[entry_max].key = buffer[0];
 
           int first_delim = fc_indexof(buffer, '|');
@@ -63,14 +72,64 @@ int main(char* args) {
   }
   fc_dsr_close(dsr, &pab);
 
-  for(int i=0; i<entry_max; i++) {
-    fc_tputc(entries[i].key);
-    fc_tputc('\n');
-    fc_tputs(entries[i].title);
-    fc_tputc('\n');
-    fc_tputs(entries[i].command);
-    fc_tputc('\n');
-  }
+  drawBackdrop();
+  layoutMenu(entry_idx);
 
+  struct MenuEntry* entry = 0;
+  while(entry == 0) {
+    entry = pickEntry();
+    if (((int)entry) == 0xFFFF) {
+      return 0;
+    } else {
+      if (((int)entry) != 0) {
+        fc_exec(entry->command);
+        entry = 0;
+        drawBackdrop();
+        layoutMenu(entry_idx);
+      }
+    }
+  }
+  return 0;
+}
+
+void drawBackdrop() {
+  vdp_memset(0, ' ', dinfo.displayWidth * dinfo.displayHeight);
+  fc_ui_gotoxy(0,0);
+  vdp_memset(0, 0xB0, dinfo.displayWidth);
+  vdp_memset((dinfo.displayHeight - 1) * dinfo.displayWidth, 0xB0, dinfo.displayWidth);
+  fc_ui_gotoxy((dinfo.displayWidth / 2) - 7,0);
+  fc_tputs(" FCMenu v1.0 ");
+}
+
+void layoutMenu(int entry_offset) {
+  int y = 3;
+  int limit = 10 > entry_max ? entry_max : 10;
+  for(int i = entry_idx; i<limit; i++) {
+    fc_ui_gotoxy(1, y);
+    if (entries[i].key == '-') {
+      // draw a separation
+      int vaddr = fc_vdp_get_cursor_addr();
+      vdp_memset(vaddr, 0xC4, 18);
+    } else {
+      fc_tputc(entries[i].key);
+      fc_ui_gotoxy(3, y);
+      fc_tputs(entries[i].title);
+    }
+    y += 2;
+  }
+  fc_ui_gotoxy(0, dinfo.displayHeight - 1);
+}
+
+struct MenuEntry* pickEntry() {
+  int limit = 10 > entry_max ? entry_max : 10;
+  unsigned int key = fc_kscan(5);
+  if (key == KEY_BACK) {
+    return (struct MenuEntry*) 0xFFFF;
+  }
+  for(int i = entry_idx; i<limit; i++) {
+    if (entries[i].key == key) {
+      return &entries[i];
+    }
+  }
   return 0;
 }
