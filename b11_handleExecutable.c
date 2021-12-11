7f03020d4705da01173ec0cb1efad9792aeb9a78
Matthew Splett <jedimatt42@protonmail.com>
2021-12-10 20:55:56 -0800

    maybe load sams exe

--- b11_handleExecutable.c
+++ b11_handleExecutable.c
@@ -23,8 +23,8 @@ int loadExecutable(const char* ext, int* cmd_type);
 int loadFromPath(const char *ext, const char *entry, int* cmd_type);
 int loadFlatFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr);
 int prepareMemory();
-int checkFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr);
-int loadPagedFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr);
+int checkFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr, int* pages);
+int loadPagedFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr, int pages);
 
 
 #define BIN 0xFCFC
@@ -35,10 +35,10 @@ int loadPagedFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename
 #define FMT_SAMS 2
 
 struct __attribute__((__packed__)) FCProgramHeader {
-    int fcfc;
-    int sams;
-    int safe;
-    int start;
+  int fcfc;
+  int sams;
+  int safe;
+  int start;
 };
 
 void popProcInfo() {
@@ -47,277 +47,327 @@ void popProcInfo() {
 
 void handleExecutable(char *ext)
 {
-    int cmd_type = 0;
+  int cmd_type = 0;
 
-    struct ProcInfo procInfo;
-    procInfo.prev = procInfoPtr;
-    procInfo.base_page = 0;
-    procInfoPtr = &procInfo;
+  struct ProcInfo procInfo;
+  procInfo.prev = procInfoPtr;
+  procInfo.base_page = 0;
+  procInfoPtr = &procInfo;
 
-    int err = loadExecutable(ext, &cmd_type);
+  int err = loadExecutable(ext, &cmd_type);
+
+  if (err) {
+    tputs_rom("unknown command: ");
+    bk_tputs_ram(ext);
+    bk_tputc('\n');
+    popProcInfo();
+    return;
+  }
+
+  if (cmd_type == BIN) {
+    identify_callback old_hook = bk_get_identify_hook();
+    int restoreDisplay = *(volatile int*)0xA004;
+    int old_nTitleLine = nTitleLine;
+    int old_oldcolors = conio_scrnCol;
+    if (restoreDisplay != 0xFCFC) {
+      // turn bar off, cause app is using the full screen.
+      nTitleLine = 0;
+      bk_setupScreen(displayWidth);
+    }
+    // Go to bank 0 to actually launch so API tables are visible.
+    err = bk_runExecutable(ext);
+
+    bk_set_identify_hook(old_hook);
+    if (restoreDisplay != 0xFCFC) {
+      conio_scrnCol = old_oldcolors;
+      nTitleLine = old_nTitleLine;
+      bk_setupScreen(displayWidth);
+    }
 
     if (err) {
-        tputs_rom("unknown command: ");
-        bk_tputs_ram(ext);
-        bk_tputc('\n');
-	popProcInfo();
-        return;
+      tputs_rom("error result: ");
+      bk_tputs_ram(bk_uint2str(err));
+      bk_tputc('\n');
     }
 
-    if (cmd_type == BIN) {
-        identify_callback old_hook = bk_get_identify_hook();
-        int restoreDisplay = *(volatile int*)0xA004;
-        int old_nTitleLine = nTitleLine;
-	int old_oldcolors = conio_scrnCol;
-        if (restoreDisplay != 0xFCFC) {
-            // turn bar off, cause app is using the full screen.
-            nTitleLine = 0;
-            bk_setupScreen(displayWidth);
-        }
-        // Go to bank 0 to actually launch so API tables are visible.
-        err = bk_runExecutable(ext);
-
-        bk_set_identify_hook(old_hook);
-        if (restoreDisplay != 0xFCFC) {
-	    conio_scrnCol = old_oldcolors;
-            nTitleLine = old_nTitleLine;
-            bk_setupScreen(displayWidth);
-        }
-
-        if (err)
-        {
-            tputs_rom("error result: ");
-            bk_tputs_ram(bk_uint2str(err));
-            bk_tputc('\n');
-        }
-
-        if (sams_total_pages) {
-            // if we didn't have sams, we wouldn't be able to get here with api_exec true.
-            int newbase = bk_free_pages(6) - 6;
-            for (int i = 0; i < 6; i++) {
-                bk_map_page(newbase + i, 0xA000 + (i * 0x1000));
-            }
-        }
+    if (sams_total_pages) {
+      // if we didn't have sams, we wouldn't be able to get here with api_exec true.
+      int newbase = bk_free_pages(6) - 6;
+      for (int i = 0; i < 6; i++) {
+	bk_map_page(newbase + i, 0xA000 + (i * 0x1000));
+      }
     }
-    popProcInfo();
+  }
+  popProcInfo();
 }
 
 char* token_cursor(char* dst, char* str, int delim) {
-    // return the pointer to the character after the delim following token in original string:str
-    // copies token into dst.
-    // returns 0 if no following string
-    if (!str)
-    {
-        dst[0] = 0;
-        return 0;
-    }
-
-    int i = 0;
-    while (str[i] != 0 && str[i] != delim)
-    {
-        dst[i] = str[i];
-        i++;
-    }
-    dst[i] = 0;
-
-    char* next = &(str[i]);
-    if (*next == delim) {
-        return next + 1;
-    }
+  // return the pointer to the character after the delim following token in original string:str
+  // copies token into dst.
+  // returns 0 if no following string
+  if (!str) {
+    dst[0] = 0;
     return 0;
+  }
+
+  int i = 0;
+  while (str[i] != 0 && str[i] != delim) {
+    dst[i] = str[i];
+    i++;
+  }
+  dst[i] = 0;
+
+  char* next = &(str[i]);
+  if (*next == delim) {
+    return next + 1;
+  }
+  return 0;
 }
 
 int loadExecutable(const char* ext, int* cmd_type) {
-    // allow ext to be fully qualified path to command
-    if (!loadFromPath(ext, 0, cmd_type)) {
-        return 0;
+  // allow ext to be fully qualified path to command
+  if (!loadFromPath(ext, 0, cmd_type)) {
+    return 0;
+  }
+
+  // look in current directory second, always... regardless of PATH
+  if (!loadFromPath(ext, currentPath, cmd_type)) {
+    return 0;
+  }
+
+  char* path = bk_vars_get(str2ram("PATH"));
+  if (path != (char*)-1) {
+    char entry[64];
+    char *cursor = token_cursor(entry, path, ';');
+    // ensure '.' at end of path entry
+    if (!bk_str_endswith(entry, str2ram("."))) {
+      bk_strcat(entry, str2ram("."));
     }
 
-    // look in current directory second, always... regardless of PATH
-    if (!loadFromPath(ext, currentPath, cmd_type)) {
-        return 0;
-    }
-
-    char* path = bk_vars_get(str2ram("PATH"));
-    if (path != (char*)-1) {
-        char entry[64];
-        char *cursor = token_cursor(entry, path, ';');
-        // ensure '.' at end of path entry
-        if (!bk_str_endswith(entry, str2ram("."))) {
-          bk_strcat(entry, str2ram("."));
-        }
-
-        while(entry[0]) {
-            if (!loadFromPath(ext, entry, cmd_type)) {
-                return 0;
-            }
-            cursor = token_cursor(entry, cursor, ';');
-        }
-        return 1;
+    while(entry[0]) {
+      if (!loadFromPath(ext, entry, cmd_type)) {
+	  return 0;
+      }
+      cursor = token_cursor(entry, cursor, ';');
     }
     return 1;
+  }
+  return 1;
 }
 
 void setDsrAndPath(const char* ext, const char* entry, struct DeviceServiceRoutine** dsr, char* path) {
-    char fullname[256];
-    fullname[0] = 0;
-    if (entry != 0) {
-        bk_strcpy(fullname, entry);
-    }
-    bk_strcat(fullname, ext);
-    bk_parsePathParam(fullname, dsr, path, PR_REQUIRED);
+  char fullname[256];
+  fullname[0] = 0;
+  if (entry != 0) {
+    bk_strcpy(fullname, entry);
+  }
+  bk_strcat(fullname, ext);
+  bk_parsePathParam(fullname, dsr, path, PR_REQUIRED);
 }
 
 void clearAddInfo(struct AddInfo* addInfoPtr) {
-    addInfoPtr->first_sector = 0;
-    addInfoPtr->eof_offset = 0;
-    addInfoPtr->flags = 0;
-    addInfoPtr->rec_length = 0;
-    addInfoPtr->records = 0;
-    addInfoPtr->recs_per_sec = 0;
+  addInfoPtr->first_sector = 0;
+  addInfoPtr->eof_offset = 0;
+  addInfoPtr->flags = 0;
+  addInfoPtr->rec_length = 0;
+  addInfoPtr->records = 0;
+  addInfoPtr->recs_per_sec = 0;
 }
 
-int loadFromPath(const char *ext, const char *entry, int* cmd_type)
-{
-    struct DeviceServiceRoutine *dsr = 0;
-    char path[256];
+int loadFromPath(const char *ext, const char *entry, int* cmd_type) {
+  struct DeviceServiceRoutine *dsr = 0;
+  char path[256];
 
-    setDsrAndPath(ext, entry, &dsr, path);
-    if (!dsr) {
-        return 1;
-    }
-
-    if (bk_runScript(dsr, path)) {
-        *cmd_type = SCRIPT;
-        return 0;
-    }
-
-    unsigned int iocode = bk_path2iocode(path);
-
-    int parent_idx = bk_lindexof(path, '.', bk_strlen(path) - 1);
-    char filename[11];
-    bk_strncpy(filename, path + parent_idx + 1, 10);
-    path[parent_idx + 1] = 0x00;
-
-    bk_lvl2_setdir(dsr->crubase, iocode, path);
-
-    // AddInfo must be in scratchpad
-    struct AddInfo *addInfoPtr = (struct AddInfo *)0x8320;
-    clearAddInfo(addInfoPtr);
-
-    unsigned int err = bk_lvl2_input(dsr->crubase, iocode, filename, 0, addInfoPtr);
-    if (err)
-    {
-        return 1;
-    }
-
-    // Check the type is PROGRAM
-    int type = (addInfoPtr->flags & 0x03);
-    if (type) {
-        *cmd_type = BIN;
-	int format = checkFormat(dsr, iocode, filename, addInfoPtr);
-        if (format == FMT_EXE) {
-          return loadFlatFormat(dsr, iocode, filename, addInfoPtr);
-	} else if (format == FMT_SAMS) {
-          return loadPagedFormat(dsr, iocode, filename, addInfoPtr);
-	}
-    }
+  setDsrAndPath(ext, entry, &dsr, path);
+  if (!dsr) {
     return 1;
-}
+  }
 
-int checkFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr) {
-    if (addInfoPtr->first_sector == 0) {
-        tputs_rom("error, file is empty.\n");
-	return FMT_ERR;
-    }
-    // peek at first block FC header
-    addInfoPtr->first_sector = 0;
-    int err = bk_lvl2_input(dsr->crubase, iocode, filename, 1, addInfoPtr);
-    if (err) {
-        tputs_rom("error reading file: ");
-        bk_tputs_ram(bk_uint2hex(err));
-        bk_tputc('\n');
-        return FMT_ERR;
-    }
-    // just after the AddInfo in scratchpad
-    struct FCProgramHeader* header = (struct FCProgramHeader*) 0x832A;
-    vdpmemread(addInfoPtr->buffer, (char*)header, sizeof(struct FCProgramHeader));
-
-    if (header->fcfc != 0xFCFC) {
-        // not an FC executable
-        return FMT_ERR;
-    }
-
-    if (header->sams == 0) {
-        return FMT_EXE;
-    } else {
-        return FMT_SAMS;
-    }
-}
-
-int loadPagedFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr) {
+  if (bk_runScript(dsr, path)) {
+    *cmd_type = SCRIPT;
     return 0;
+  }
+
+  unsigned int iocode = bk_path2iocode(path);
+
+  int parent_idx = bk_lindexof(path, '.', bk_strlen(path) - 1);
+  char filename[11];
+  bk_strncpy(filename, path + parent_idx + 1, 10);
+  path[parent_idx + 1] = 0x00;
+
+  bk_lvl2_setdir(dsr->crubase, iocode, path);
+
+  // AddInfo must be in scratchpad
+  struct AddInfo *addInfoPtr = (struct AddInfo *)0x8320;
+  clearAddInfo(addInfoPtr);
+
+  unsigned int err = bk_lvl2_input(dsr->crubase, iocode, filename, 0, addInfoPtr);
+  if (err) {
+    return 1;
+  }
+
+  // Check the type is PROGRAM
+  int type = (addInfoPtr->flags & 0x03);
+  if (type) {
+    *cmd_type = BIN;
+    int pages = 0;
+    int format = checkFormat(dsr, iocode, filename, addInfoPtr, &pages);
+    if (format == FMT_EXE) {
+      return loadFlatFormat(dsr, iocode, filename, addInfoPtr);
+    } else if (format == FMT_SAMS) {
+      return loadPagedFormat(dsr, iocode, filename, addInfoPtr, pages);
+    }
+  }
+  return 1;
+}
+
+int checkFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr, int* pages) {
+  if (addInfoPtr->first_sector == 0) {
+    tputs_rom("error, file is empty.\n");
+    return FMT_ERR;
+  }
+  // peek at first block FC header
+  addInfoPtr->first_sector = 0;
+  int err = bk_lvl2_input(dsr->crubase, iocode, filename, 1, addInfoPtr);
+  if (err) {
+    tputs_rom("error reading file: ");
+    bk_tputs_ram(bk_uint2hex(err));
+    bk_tputc('\n');
+    return FMT_ERR;
+  }
+  // just after the AddInfo in scratchpad
+  struct FCProgramHeader* header = (struct FCProgramHeader*) 0x832A;
+  vdpmemread(addInfoPtr->buffer, (char*)header, sizeof(struct FCProgramHeader));
+
+  if (header->fcfc != 0xFCFC) {
+    // not an FC executable
+    return FMT_ERR;
+  }
+
+  if (header->sams == 0) {
+  *pages = 6;
+    return FMT_EXE;
+  } else {
+    *pages = header->sams;
+    return FMT_SAMS;
+  }
+}
+
+int allocatePages(int pages) {
+  if (sams_next_page + pages > sams_total_pages) {
+    return 1;  // don't allow nested execution if we don't have space
+  }
+  int pageStart = bk_alloc_pages(pages);
+  procInfoPtr->base_page = pageStart;
+  if (pageStart == -1) {
+    return 1; // no more pages, fail the same way
+  }
+  return 0;
 }
 
 /* return true if there is a problem preparing memory */
 int prepareMemory() {
-    if (sams_next_page + 6 > sams_total_pages) {
-        return 1;  // don't allow nested execution if we don't have space
-    }
-    int pageStart = bk_alloc_pages(6);
-    procInfoPtr->base_page = pageStart;
-    if (pageStart == -1) {
-        return 1; // no more pages, fail the same way
-    }
-    // should be good to map upper memory expansion to the new pages.
-    for (int i = 0; i < 6; i++) {
-        bk_map_page(pageStart + i, 0xA000 + (i * 0x1000));
-    }
-    return 0;
+  if (allocatePages(6)) {
+    return 1;
+  }
+  // should be good to map upper memory expansion to the new pages.
+  for (int i = 0; i < 6; i++) {
+    bk_map_page(procInfoPtr->base_page + i, 0xA000 + (i * 0x1000));
+  }
+  return 0;
 }
 
-int loadFlatFormat(struct DeviceServiceRoutine *dsr, int iocode, char *filename, struct AddInfo *addInfoPtr)
-{
-    if (sams_total_pages) { // if sams exists
-        if (prepareMemory()) {
-            return 1;
-        }
+int loadPagedFormat(struct DeviceServiceRoutine* dsr, int iocode, char* filename, struct AddInfo* addInfoPtr, int pages) {
+  clearAddInfo(addInfoPtr);
+  bk_lvl2_input(dsr->crubase, iocode, filename, 0, addInfoPtr);
+
+  int totalBlocks = addInfoPtr->first_sector;
+  int extraPages = (totalBlocks >> 4) - pages; 
+  if (sams_total_pages) {
+    if (allocatePages(pages + extraPages)) {
+      return 1;
     }
-    char *cpuAddr = (char *)0xA000;
+  } else {
+    return 1;
+  }
 
-    // need to repair ?addInfo? to state before reading the first block
-    clearAddInfo(addInfoPtr);
-    bk_lvl2_input(dsr->crubase, iocode, filename, 0, addInfoPtr);
+  int blockId = 0;
+  int page = 0;
+  while(page < pages) {
+    int loadAddr = (page % 2 == 0) ? 0xA000 : 0xB000;
+    bk_map_page(procInfoPtr->base_page + page, loadAddr);
+    addInfoPtr->first_sector = blockId;
+    int err = bk_lvl2_input(dsr->crubase, iocode, filename, 16, addInfoPtr);
+    blockId += 16;
+	
+    // every other page goes into A000 or B000
+    vdpmemread(addInfoPtr->buffer, (char*)loadAddr, 4096);
 
-    int totalBlocks = addInfoPtr->first_sector;
-    // Allow the file to be larger, but only load the first 24K.
-    if (totalBlocks > 96) {
-        totalBlocks = 96;
+    page++;
+  }
+  // expect the first 2 pages mapped in
+  bk_map_page(procInfoPtr->base_page, 0xA000);
+  if (pages > 1) {
+    bk_map_page(procInfoPtr->base_page + 1, 0xB000);
+  }
+
+  // then load the remaining data/bss blocks from disk
+  //   totalBlocks - blockId -> 0xC000
+  int cpuAddr = 0xC000;
+  while(blockId < totalBlocks) {
+    bk_map_page(procInfoPtr->base_page + page, cpuAddr);
+    addInfoPtr->first_sector = blockId;
+    int err = bk_lvl2_input(dsr->crubase, iocode, filename, 16, addInfoPtr);
+    blockId += 16;
+    
+    vdpmemread(addInfoPtr->buffer, (char*)cpuAddr, 4096);
+    if (cpuAddr != 0xF000) {
+      cpuAddr += 0x1000;
+    }
+  }
+
+  return 0;
+}
+
+int loadFlatFormat(struct DeviceServiceRoutine *dsr, int iocode, char *filename, struct AddInfo *addInfoPtr) {
+  if (sams_total_pages) { // if sams exists
+    if (prepareMemory()) {
+        return 1;
+    }
+  }
+  char *cpuAddr = (char *)0xA000;
+
+  // need to repair ?addInfo? to state before reading the first block
+  clearAddInfo(addInfoPtr);
+  bk_lvl2_input(dsr->crubase, iocode, filename, 0, addInfoPtr);
+
+  int totalBlocks = addInfoPtr->first_sector;
+  // Allow the file to be larger, but only load the first 24K.
+  if (totalBlocks > 96) {
+    totalBlocks = 96;
+  }
+
+  int blockId = 0;
+  while (blockId < totalBlocks) {
+    int blk_cnt = totalBlocks - blockId;
+    if (blk_cnt > 17) {
+      blk_cnt = 17;
+    }
+    int load_size = blk_cnt << 8;
+
+    addInfoPtr->first_sector = blockId;
+    int err = bk_lvl2_input(dsr->crubase, iocode, filename, blk_cnt, addInfoPtr);
+    if (err) {
+      tputs_rom("error reading file: ");
+      bk_tputs_ram(bk_uint2hex(err));
+      bk_tputc('\n');
+      return 1;
     }
 
-    int blockId = 0;
-    while (blockId < totalBlocks)
-    {
-        int blk_cnt = totalBlocks - blockId;
-        if (blk_cnt > 17) {
-            blk_cnt = 17;
-        }
-        int load_size = blk_cnt << 8;
+    vdpmemread(addInfoPtr->buffer, cpuAddr, load_size);
 
-        addInfoPtr->first_sector = blockId;
-        int err = bk_lvl2_input(dsr->crubase, iocode, filename, blk_cnt, addInfoPtr);
-        if (err)
-        {
-            tputs_rom("error reading file: ");
-            bk_tputs_ram(bk_uint2hex(err));
-            bk_tputc('\n');
-            return 1;
-        }
-
-        vdpmemread(addInfoPtr->buffer, cpuAddr, load_size);
-
-        cpuAddr += load_size;
-        blockId += blk_cnt;
-    }
-    return 0;
+    cpuAddr += load_size;
+    blockId += blk_cnt;
+  }
+  return 0;
 }
