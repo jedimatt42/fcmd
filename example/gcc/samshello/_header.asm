313c5364553a46392853d0c4454307d21dbe9c2d
Matthew Splett <jedimatt42@protonmail.com>
2021-12-31 14:16:21 -0800

    2 bank, 4 page sams page header

--- example/gcc/samshello/_header.asm
+++ example/gcc/samshello/_header.asm
@@ -1,5 +1,5 @@
     REF fc_main
     data 0xFCFC   ; Flag for valid Force Command binary
-    data 0x0004   ; SAMS page count - 0x0000 means simply / no sams required
+    data 0x0004   ; SAMS page count - 0x0004 means sams required, 2 banks of 2 pages - 4
     data 0xFCFC   ; Flag that program promises to use screen safely
     data fc_main     ; program start address
