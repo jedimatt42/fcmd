03bde379295265e3bc172aae0dd2962716ba0034
Matthew Splett <jedimatt42@protonmail.com>
2024-03-17 23:04:07 -0700

    let deploy.sh detect it's home

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Find the location of this script
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+# Set the current directory to that of the script
+cd $SCRIPT_DIR
+
 
 if [ -z ${CART_DEPLOY_DIR:-} ]; then
   echo "export CART_DEPLOY_DIR to directory you want to copy .bin and .rpk files to"
