61469c6f830d75f0d84a7bb68ded32d09e5bceba
Matthew Splett <jedimatt42@protonmail.com>
2024-08-31 16:02:35 -0700

    debugging

--- b4_handleAlias.c
+++ b4_handleAlias.c
@@ -10,29 +10,29 @@
 #include "b8_terminal.h"
 
 int onAliasEntry(struct DictEntry* entry) {
-  // if (entry->type == DE_TYPE_ALIAS && request_break == 0) {
-  //   bk_tputs_ram(entry->data);
-  //   bk_tputc(':');
-  //   bk_tputs_ram(entry->data + entry->keylen + 1);
-  //   bk_tputc('\n');
-  // }
+  if (entry->type == DE_TYPE_ALIAS && request_break == 0) {
+    bk_tputs_ram(entry->data);
+    bk_tputc(':');
+    bk_tputs_ram(entry->data + entry->keylen + 1);
+    bk_tputc('\n');
+  }
   bk_tputc('-');
-  // bk_tputs_ram(bk_uint2str((int) entry));
+  bk_tputs_ram(bk_uint2str((int) entry));
   bk_tputc('\n');
   return 0;
 }
 
 void handleAlias() {
   char* name = bk_strtokpeek(0, ' ');
-  int list = name == 0;
-
-  if (list) {
-    dict_visit(&system_dict, onAliasEntry);
-    bk_tputc('\n');
-  } else {
+  if (name) {
     // consume name
     bk_strtok(0, ' ');
     char* value = bk_strtok(0, 0);
+    // if there is no value, it will unset the alias
     alias_set(name, value);
+  } else {
+    // if no parameters were provided, list all aliases
+    dict_visit(&system_dict, onAliasEntry);
+    bk_tputc('\n');
   }
 }
