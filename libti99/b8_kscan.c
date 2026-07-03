86896688f7fa3ce1c4e0a9f6f556fd7ef3f5831b
Matthew Splett <jedimatt42@protonmail.com>
2026-07-03 12:56:28 -0700

    when console rom is banked out, we have to use another tramp to call into console kscan

--- libti99/b8_kscan.c
+++ libti99/b8_kscan.c
@@ -1,7 +1,51 @@
 #include "kscan.h"
+#include <banks.h>
+
+/*
+When the console rom is banked, we have to carefully craft code in RAM >8320 to
+make the call and return to the FCMD bank and code. If we take advantage of using
+the GPLWS, we can prepare registers.
+*/
+
+#ifdef CONSOLE_BANK
+
+void crom_tramp() {
+    __asm__(
+        "LWPI >83E0\n\t"
+		"LI   r1, 63\n\t"
+		"MOV  r1, @>0000\n\t"
+		"BL   @>000E\n\t"
+        "LI   r1, 8\n\t"
+		"MOV  r1, @>0000\n\t"
+		"LWPI >8300\n\t"
+	);
+}
 
 unsigned int term_kscan(unsigned int mode) {
 	KSCAN_MODE = mode;
-	__asm__("LWPI >83E0\n\tBL @>000E\n\tLWPI >8300");
+
+    int* src = (int*)crom_tramp;
+	int* end = (int*)term_kscan;
+	int* dst = (int*)0x8320;
+	while(src < end) {
+		*dst++ = *src++;
+	}
+
+	((void (*)(void))0x8320)();
+
 	return KSCAN_KEY;
 }
+
+#else
+
+unsigned int term_kscan(unsigned int mode) {
+	KSCAN_MODE = mode;
+	__asm__(
+		"LWPI >83E0\n\t"
+		"BL @>000E\n\t"
+		"LWPI >8300\n\t"
+	);
+	return KSCAN_KEY;
+}
+
+#endif
